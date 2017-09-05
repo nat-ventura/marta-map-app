82ff9b09718c9003e03fe3fbdbdae2a62af7862a
Natalie Villasana <navillasa@gmail.com>
2017-09-05 14:23:38 -0400

    adds Destination.js to Components/ and create martaDataGrabber in Dashboard.js

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Card from './Card.js';
+
+const getMartaData = () => {
+    return fetch('http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=2c514350-0c26-47dd-b872-7936af81c8e1', {
+        method: 'get'
+    }).then( function (res) {
+        return res.json()
+    }).catch( function (err) {
+        alert(err);
+    });
+}
+
+class Dashboard extends Component {
+    constructor(prop) {
+        super(props);
+        this.state = {
+            martaData: [],
+            localTime: new Date(),
+            emptyArray: 0
+        };
+    }
+}
+
+componentWillMount() {
+
+    this.martaDataGrabber = setInterval( () => {
+        getMartaData().then((jsonData) => {
+            let new_data = [];
+            let emptySignal = [{DESTINATION: 'No trains available at this time.', NEXT_ARR: ' '}];
+            jsonData.map( (data) => {
+                if (data.DIRECTION == this.props.direction && data.STATION == this.props.station) {
+                    new_data.push(data)
+                }
+                return data;
+            })
+            if (new_data.length > 0) {
+                if (new_data.length >= this.state.martaData.length || new_data[0].DIRECTION != this.state.martaData[0].DIRECTION) {
+                    this.setState({
+                        martaData: new_data,
+                        emptyArray: 0
+                    });
+                }
+            } else if (new_data.length == 0) {
+                let size = this.state.emptyArray + 1;
+                this.setState({
+                    emptyArray: size
+                })
+                if (this.state.emptyArray >= 3) {
+                    this.setState({
+                        martaData: emptySignal
+                    })
+                }
+            }
+        })
+        this.setState({
+            localTime: new Date()
+        })
+    }, 1000);
+}
+
---
+++ src/Components/Destination.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Destination = ({destination}) => {
+    return <h3 className='class-title'>{destination}</h3>
+}
+
+export default Destination;
