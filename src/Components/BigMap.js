9b6fe6a7de92bdc7f13a79fe0b88e1d327e801a2
Natalie Villasana <navillasa@gmail.com>
2017-09-06 19:09:32 -0400

    turn STATION_LIST into STATIONS dictionary... probably not the most efficient way

--- src/Components/BigMap.js
+++ src/Components/BigMap.js
@@ -4,40 +4,40 @@ import StationOnMap from './StationOnMap.js';
 import axios from 'axios';
 
 const MARTA_URL = 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=2c514350-0c26-47dd-b872-7936af81c8e1';
-const STATION_LIST = [
-                    'AIRPORT STATION',
-                    'ART CENTER STATION',
-                    'ASHBY STATION',
-                    'AVONDALE STATION',
-                    'BANKHEAD STATION',
-                    'BROOKHAVEN STATION',
-                    'BUCKHEAD STATION',
-                    'CHAMBLEE STATION',
-                    'CIVIC CENTER STATION',
-                    'COLLEGE PARK STATION',
-                    'DECATUR STATION',
-                    'PEACHTREE CENTER STATION',
-                    'DORAVILLE STATION',
-                    'DUNWOODY STATION',
-                    'EAST LAKE STATION',
-                    'EDGEWOOD STATION',
-                    'FIVE POINTS STATION',
-                    'GARNETT STATION',
-                    'H. E. HOLMES STATION',
-                    'INDIAN CREEK STATION',
-                    'KENSINGTON STATION',
-                    'KING MEMORIAL STATION',
-                    'LENOX STATION',
-                    'LINDBERGH CENTER STATION',
-                    'MEDICAL CENTER STATION',
-                    'MIDTOWN STATION',
-                    'NORTH AVENUE STATION',
-                    'NORTH SPRINGS STATION',
-                    'OAKLAND CITY STATION',
-                    'SANDY SPRINGS STATION',
-                    'WEST END STATION',
-                    'WEST LAKE STATION'
-                ]
+const STATIONS = {
+                    'AIRPORT STATION': [],
+                    'ART CENTER STATION': [],
+                    'ASHBY STATION': [],
+                    'AVONDALE STATION': [],
+                    'BANKHEAD STATION': [],
+                    'BROOKHAVEN STATION': [],
+                    'BUCKHEAD STATION': [],
+                    'CHAMBLEE STATION': [],
+                    'CIVIC CENTER STATION': [],
+                    'COLLEGE PARK STATION': [],
+                    'DECATUR STATION': [],
+                    'PEACHTREE CENTER STATION': [],
+                    'DORAVILLE STATION': [],
+                    'DUNWOODY STATION': [],
+                    'EAST LAKE STATION': [],
+                    'EDGEWOOD STATION': [],
+                    'FIVE POINTS STATION': [],
+                    'GARNETT STATION': [],
+                    'H. E. HOLMES STATION': [],
+                    'INDIAN CREEK STATION': [],
+                    'KENSINGTON STATION': [],
+                    'KING MEMORIAL STATION': [],
+                    'LENOX STATION': [],
+                    'LINDBERGH CENTER STATION': [],
+                    'MEDICAL CENTER STATION': [],
+                    'MIDTOWN STATION': [],
+                    'NORTH AVENUE STATION': [],
+                    'NORTH SPRINGS STATION': [],
+                    'OAKLAND CITY STATION': [],
+                    'SANDY SPRINGS STATION': [],
+                    'WEST END STATION': [],
+                    'WEST LAKE STATION': []
+                    }
 
 
 const getMartaData = () => {
@@ -59,41 +59,41 @@ class BigMap extends Component {
     }
 
     componentWillMount() {
-        // this.martaDataGrabber = setInterval( () => {
-        //     getMartaData().then((jsonData) => {
-        //         let trainObjectArray = []
-        //         let emptySignal = [{DESTINATION: 'No trains available at this time.', NEXT_ARR: ' '}]
-        //         jsonData.map((trainObject) => {
-        //             if (trainObject.DIRECTION === this.props.direction && trainObject.STATION === this.props.station) {
-        //                 trainObjectArray.push(trainObject)
-        //             }
-        //             // console.log(trainObject);
-        //             return trainObject;
-        //         })
-        //         if (trainObjectArray.length > 0) {
-        //             if (trainObjectArray.length >= this.state.martaData.length || trainObjectArray[0].DIRECTION !== this.state.martaData[0].DIRECTION) {
-        //                 this.setState({
-        //                     martaData: trainObjectArray,
-        //                     emptyArray: 0
-        //                 });
-        //                 // console.log('************************ HELLO HELLO', this.state.martaData)
-        //             }
-        //         } else if (trainObjectArray.length === 0) {
-        //             let size = this.state.emptyArray + 1;
-        //             this.setState({
-        //                 emptyArray: size
-        //             })
-        //             if (this.state.emptyArray >= 3) {
-        //                 this.setState({
-        //                     martaData: emptySignal
-        //                 })
-        //             }
-        //         }
-        //     })
-        //     this.setState({
-        //         localTime: new Date()
-        //     })
-        // }, 1000);
+        this.martaDataGrabber = setInterval( () => {
+            getMartaData().then((jsonData) => {
+                let trainObjectArray = []
+                let emptySignal = [{DESTINATION: 'No trains available at this time.', NEXT_ARR: ' '}]
+                jsonData.map((trainObject) => {
+                    if (trainObject.DIRECTION === this.props.direction && trainObject.STATION === this.props.station) {
+                        trainObjectArray.push(trainObject)
+                    }
+                    // console.log(trainObject);
+                    return trainObject;
+                })
+                if (trainObjectArray.length > 0) {
+                    if (trainObjectArray.length >= this.state.martaData.length || trainObjectArray[0].DIRECTION !== this.state.martaData[0].DIRECTION) {
+                        this.setState({
+                            martaData: trainObjectArray,
+                            emptyArray: 0
+                        });
+                        // console.log('************************ HELLO HELLO', this.state.martaData)
+                    }
+                } else if (trainObjectArray.length === 0) {
+                    let size = this.state.emptyArray + 1;
+                    this.setState({
+                        emptyArray: size
+                    })
+                    if (this.state.emptyArray >= 3) {
+                        this.setState({
+                            martaData: emptySignal
+                        })
+                    }
+                }
+            })
+            this.setState({
+                localTime: new Date()
+            })
+        }, 1000);
     }
 
     componentWillUnmount() {
@@ -117,7 +117,23 @@ class BigMap extends Component {
         return (
             <div className='row'>
                 <div className='style-me'>
-                    {STATION_LIST.map( (station, idx) => {
+                    {/*
+                    right now this is mapping through the static
+                    station list-- but the part that actually needs
+                    to be live/checking-- is if received trainObjects
+                    have the same station ID as the current station.
+                    if trainObject.station === STATIONS[trainObject.station]:
+                        STATIONS[trainObject.station].push(trainObject.train_id)
+                    if trainObject.station === STATION_LIST[i] {
+                       then append that trainObject's TRAIN_ID
+                       to the STATION_ON_MAP div.
+                       can you make headings values in a dictionary?
+                    
+                    }
+                    hm this is making me think that each station on map
+                    should be a dictionary with all of the train_ids.
+                    */}
+                    {Object.keys(STATIONS).map( (station, idx) => {
                         return (
                             <div key={idx}>
                                 <StationOnMap station={station}/>
