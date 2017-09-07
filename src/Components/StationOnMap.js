209d97cdd637915f50482dec07afe560d2d73020
Natalie Villasana <navillasa@gmail.com>
2017-09-06 22:40:45 -0400

    trying to figure out how to add each trainObject's TRAIN_ID to a div appended to StationOnMap

--- src/Components/StationOnMap.js
+++ src/Components/StationOnMap.js
@@ -2,7 +2,10 @@ import React from 'react';
 // import BigMap from './BigMap.js';
 
 const StationOnMap = (props) => {
-    return <h3 className='little-station'>{props.station}</h3>
+    return
+        <div>
+            <h3 className='little-station'>{props.station}</h3>
+        </div>
 }
 
 export default StationOnMap;
