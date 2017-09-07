8129a9e60f16f21ee8743805a929287c544b3959
Natalie Villasana <navillasa@gmail.com>
2017-09-07 11:37:09 -0400

    moves STATION_DICT to 'stations' as part of state

--- src/Components/BigMap.js
+++ src/Components/BigMap.js
@@ -4,40 +4,40 @@ import StationOnMap from './StationOnMap.js';
 import axios from 'axios';
 
 const MARTA_URL = 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=2c514350-0c26-47dd-b872-7936af81c8e1';
-const STATION_DICT = {
-                    'AIRPORT STATION': [],
-                    'ART CENTER STATION': [],
-                    'ASHBY STATION': [],
-                    'AVONDALE STATION': [],
-                    'BANKHEAD STATION': [],
-                    'BROOKHAVEN STATION': [],
-                    'BUCKHEAD STATION': [],
-                    'CHAMBLEE STATION': [],
-                    'CIVIC CENTER STATION': [],
-                    'COLLEGE PARK STATION': [],
-                    'DECATUR STATION': [],
-                    'PEACHTREE CENTER STATION': [],
-                    'DORAVILLE STATION': [],
-                    'DUNWOODY STATION': [],
-                    'EAST LAKE STATION': [],
-                    'EDGEWOOD STATION': [],
-                    'FIVE POINTS STATION': [],
-                    'GARNETT STATION': [],
-                    'H. E. HOLMES STATION': [],
-                    'INDIAN CREEK STATION': [],
-                    'KENSINGTON STATION': [],
-                    'KING MEMORIAL STATION': [],
-                    'LENOX STATION': [],
-                    'LINDBERGH CENTER STATION': [],
-                    'MEDICAL CENTER STATION': [],
-                    'MIDTOWN STATION': [],
-                    'NORTH AVENUE STATION': [],
-                    'NORTH SPRINGS STATION': [],
-                    'OAKLAND CITY STATION': [],
-                    'SANDY SPRINGS STATION': [],
-                    'WEST END STATION': [],
-                    'WEST LAKE STATION': []
-                    }
+// const STATION_DICT = {
+//                     'AIRPORT STATION': [],
+//                     'ART CENTER STATION': [],
+//                     'ASHBY STATION': [],
+//                     'AVONDALE STATION': [],
+//                     'BANKHEAD STATION': [],
+//                     'BROOKHAVEN STATION': [],
+//                     'BUCKHEAD STATION': [],
+//                     'CHAMBLEE STATION': [],
+//                     'CIVIC CENTER STATION': [],
+//                     'COLLEGE PARK STATION': [],
+//                     'DECATUR STATION': [],
+//                     'PEACHTREE CENTER STATION': [],
+//                     'DORAVILLE STATION': [],
+//                     'DUNWOODY STATION': [],
+//                     'EAST LAKE STATION': [],
+//                     'EDGEWOOD STATION': [],
+//                     'FIVE POINTS STATION': [],
+//                     'GARNETT STATION': [],
+//                     'H. E. HOLMES STATION': [],
+//                     'INDIAN CREEK STATION': [],
+//                     'KENSINGTON STATION': [],
+//                     'KING MEMORIAL STATION': [],
+//                     'LENOX STATION': [],
+//                     'LINDBERGH CENTER STATION': [],
+//                     'MEDICAL CENTER STATION': [],
+//                     'MIDTOWN STATION': [],
+//                     'NORTH AVENUE STATION': [],
+//                     'NORTH SPRINGS STATION': [],
+//                     'OAKLAND CITY STATION': [],
+//                     'SANDY SPRINGS STATION': [],
+//                     'WEST END STATION': [],
+//                     'WEST LAKE STATION': []
+//                     }
 
 const getMartaData = () => {
     return axios.get(MARTA_URL)
@@ -53,19 +53,51 @@ class BigMap extends Component {
         this.state = {
             martaData: [],
             localTime: new Date(),
-            emptyArray: 0
+            emptyArray: 0,
+            stations: {
+                'AIRPORT STATION': [],
+                'ART CENTER STATION': [],
+                'ASHBY STATION': [],
+                'AVONDALE STATION': [],
+                'BANKHEAD STATION': [],
+                'BROOKHAVEN STATION': [],
+                'BUCKHEAD STATION': [],
+                'CHAMBLEE STATION': [],
+                'CIVIC CENTER STATION': [],
+                'COLLEGE PARK STATION': [],
+                'DECATUR STATION': [],
+                'PEACHTREE CENTER STATION': [],
+                'DORAVILLE STATION': [],
+                'DUNWOODY STATION': [],
+                'EAST LAKE STATION': [],
+                'EDGEWOOD STATION': [],
+                'FIVE POINTS STATION': [],
+                'GARNETT STATION': [],
+                'H. E. HOLMES STATION': [],
+                'INDIAN CREEK STATION': [],
+                'KENSINGTON STATION': [],
+                'KING MEMORIAL STATION': [],
+                'LENOX STATION': [],
+                'LINDBERGH CENTER STATION': [],
+                'MEDICAL CENTER STATION': [],
+                'MIDTOWN STATION': [],
+                'NORTH AVENUE STATION': [],
+                'NORTH SPRINGS STATION': [],
+                'OAKLAND CITY STATION': [],
+                'SANDY SPRINGS STATION': [],
+                'WEST END STATION': [],
+                'WEST LAKE STATION': []
+                }
         };
     }
 
     componentWillMount() {
-        this.martaDataGrabber = setInterval( () => {
+        this.intervalSetter = setInterval( () => {
             getMartaData().then((jsonData) => {
                 jsonData.map((trainObject) => {
-                    if (Object.keys(STATION_DICT).includes(trainObject.STATION)) {
-                        STATION_DICT[trainObject.STATION].push(trainObject.TRAIN_ID);
-                    }
+                    this.state.stations[trainObject.STATION].push(trainObject.TRAIN_ID);
                 });
-                console.log(STATION_DICT);
+                // console.log(STATION_DICT);
                 this.setState({
                     localTime: new Date()
                 })
@@ -74,13 +106,29 @@ class BigMap extends Component {
     }
 
     componentWillUnmount() {
-        clearInterval(this.martaDataGrabber);
+        clearInterval(this.intervalSetter);
     }
 
     render() {
         return (
-            <div className='row'>
-                <div className='style-me'>
+                <div>
+                    {Object.keys(this.state.stations).map( (station, idx) => {
+                        return (
+                            <div key={idx}>
+                                <StationOnMap station={station} />
+                            </div>
+                        )
+                    })}
+                </div>
+        )
+    }
+}
+
+export default BigMap;
+
+
+
+
                     {/*
                     hm this is making me think that each station on map
                     should be a dictionary with all of the train_ids.
@@ -92,17 +140,3 @@ class BigMap extends Component {
                             </div>
                         )
                     })} */}
-                    {Object.keys(STATION_DICT).map( (station, idx) => {
-                        return (
-                            <div key={idx}>
-                                <StationOnMap station={station} />
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        )
-    }
-}
-
-export default BigMap;
